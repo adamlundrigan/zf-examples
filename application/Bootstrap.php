03ce9ec773de3d3806e0895a81e1b59192cb817a
Adam Lundrigan <adamlundrigan@cdli.ca>
2011-06-17 15:07:49 -0230

    Added resource injector (to allow $this->di from action controller)

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -2,7 +2,20 @@
 
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
-
+        
+    /**
+     * Set up resource injector
+     * @see http://weierophinney.net/matthew/archives/235-A-Simple-Resource-Injector-for-ZF-Action-Controllers.html
+     */
+    protected function _initResourceInjector()
+    {
+        $this->bootstrap('di');        
+        Zend_Controller_Action_HelperBroker::addHelper(
+            new My_ResourceInjector(array(
+                'di'
+            ))
+        );
+    }
 
 }
 
---
+++ library/My/ResourceInjector.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * Bootstrap Resource Injector
+ * @author Matthew Weier O'Phinney
+ * @see http://weierophinney.net/matthew/archives/235-A-Simple-Resource-Injector-for-ZF-Action-Controllers.html
+ */
+class My_ResourceInjector extends Zend_Controller_Action_Helper_Abstract
+{
+    protected $_resources;
+
+    public function __construct(array $resources = array())
+    {
+        $this->_resources = $resources;
+    }
+ 
+    public function preDispatch()
+    {
+        $bootstrap  = $this->getBootstrap();
+        $controller = $this->getActionController();
+        foreach ($this->_resources as $name) {
+            if ($bootstrap->hasResource($name)) {
+                $controller->$name = $bootstrap->getResource($name);
+            }
+        }
+    }
+ 
+    public function getBootstrap()
+    {
+        return $this->getFrontController()->getParam('bootstrap');
+    }
+}
