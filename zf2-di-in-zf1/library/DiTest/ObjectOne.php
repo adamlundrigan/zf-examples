9bf3a7362f3c5317873cc1352ab04ca3a206de0a
Adam Lundrigan <adam@lundrigan.ca>
2011-08-05 11:48:10 -0230

    Added docblock annotations to test objects

--- zf2-di-in-zf1/library/DiTest/ObjectOne.php
+++ zf2-di-in-zf1/library/DiTest/ObjectOne.php
@@ -1,24 +1,53 @@
 <?php
 namespace DiTest;
 
+/**
+ * Dependency Injection Test Object #1
+ * @author Adam Lundrigan <adam@lundrigan.ca>
+ */
 class ObjectOne
 {
+    /**
+     * Instance Identifier
+     * @var string
+     */
     protected $_id = null;
+    
+    /**
+     * ObjectTwo Dependency
+     * @var ObjectTwo
+     */
     protected $_objTwo = null;
     
+    /**
+     * Create new Instance
+     */
     public function __construct()
     {
         $this->_id = uniqid();
     }
     
+    /**
+     * Set Dependency
+     * @param ObjectTwo $o
+     * @return ObjectOne 
+     */
     public function setTwo(ObjectTwo $o)
     {
         $this->_objTwo = $o;
+        return $this;
     }
     
+    /**
+     * String Representation of Instance
+     * @return string
+     */
     public function __toString()
     {
-        return 'Object One = ' . $this->_id ."\n"
-               . ' - ' . $this->_objTwo;
+        ob_start();
+        var_dump($this);
+        $contents = ob_get_contents();
+        ob_clean();
+        return $contents;
     }
 }
--- zf2-di-in-zf1/library/DiTest/ObjectTwo.php
+++ zf2-di-in-zf1/library/DiTest/ObjectTwo.php
@@ -1,17 +1,37 @@
 <?php
 namespace DiTest;
 
+/**
+ * Dependency Injection Test Object #2
+ * @author Adam Lundrigan <adam@lundrigan.ca>
+ */
 class ObjectTwo
 {
+    /**
+     * Instance Identifier
+     * @var mixed
+     */
     protected $_id = null;
     
+    /**
+     * Create new Instance
+     * @param mixed $id 
+     */
     public function __construct($id)
     {
         $this->_id = $id;
     }
     
+    /**
+     * String Representation of Instance
+     * @return string
+     */
     public function __toString()
     {
-        return 'Object Two = ' . $this->_id ."\n";
+        ob_start();
+        var_dump($this);
+        $contents = ob_get_contents();
+        ob_clean();
+        return $contents;
     }
 }
